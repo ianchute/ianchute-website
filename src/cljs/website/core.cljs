d7da40a4e3805f3dcdd1e884b87aa0ee82a80f7d
Ian Chu Te <ianchute@hotmail.com>
2017-10-08 13:51:30 +0800

    Frontend cleanup.

--- src/cljs/website/core.cljs
+++ src/cljs/website/core.cljs
@@ -9,25 +9,20 @@
             [ajax.core :refer [GET POST]])
   (:import goog.History))
 
-(defn nav-link [uri title page collapsed?]
+(defn nav-link [uri title page]
   [:li.nav-item
    {:class (when (= page (session/get :page)) "active")}
    [:a.nav-link
-    {:href uri
-     :on-click #(reset! collapsed? true)} title]])
+    {:href uri} title]])
 
 (defn navbar []
-  (let [collapsed? (r/atom true)]
-    (fn []
-      [:nav.navbar.navbar-dark.bg-primary
-       [:button.navbar-toggler.hidden-sm-up
-        {:on-click #(swap! collapsed? not)} "☰"]
-       [:div.collapse.navbar-toggleable-xs
-        (when-not @collapsed? {:class "in"})
-        [:a.navbar-brand {:href "#/"} "website"]
-        [:ul.nav.navbar-nav
-         [nav-link "#/" "Home" :home collapsed?]
-         [nav-link "#/about" "About" :about collapsed?]]]])))
+  (fn []
+    [:nav.navbar.navbar-default
+      [:div.collapse.navbar-toggleable-xs.in
+      [:a.navbar-brand {:href "#/"} "website"]
+      [:ul.nav.navbar-nav
+        [nav-link "#/" "Home" :home]
+        [nav-link "#/about" "About" :about]]]]))
 
 (defn about-page []
   [:div.container
