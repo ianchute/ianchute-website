2c8c0af3d17e55f8aae8e2d70df5f680c40ef7dc
Ian Chu Te <ianchute@hotmail.com>
2017-10-08 18:37:07 +0800

    Design v0.0

--- src/cljs/website/core.cljs
+++ src/cljs/website/core.cljs
@@ -19,27 +19,92 @@
   (fn []
     [:nav.navbar.navbar-default
       [:div.collapse.navbar-toggleable-xs.in
-      [:a.navbar-brand {:href "#/"} "website"]
+      [:a.navbar-brand {:href "#/"} 
+        [:img.logo {:src (str js/context "/img/icon.png")}] 
+        "Ian Chu Te"]
       [:ul.nav.navbar-nav
         [nav-link "#/" "Home" :home]
+        ; [nav-link "#/experiments" "Experiments" :experiments]
+        ; [nav-link "#/games" "Games" :games]
         [nav-link "#/about" "About" :about]]]]))
-
-(defn about-page []
-  [:div.container
-   [:div.row
-    [:div.col-md-12
-     [:img {:src (str js/context "/img/warning_clojure.png")}]]]])
 
 (defn home-page []
   [:div.container
-   (when-let [docs (session/get :docs)]
-     [:div.row>div.col-sm-12
-      [:div {:dangerouslySetInnerHTML
-             {:__html (md->html docs)}}]])])
+    [:div.row.card-main
+      [:div.col-md-4.card-section
+        [:p.speech.welcome "Welcome to my website!"]
+        [:img.profile {:src (str js/context "/img/me.jpg")}]
+        [:img.profile-sticker {:src (str js/context "/img/sticker.png")}] 
+        [:h2.my-name "Ian Herve U. Chu Te"]
+        [:h4 {:style {:font-style "italic"}} "Data Scientist, Web Developer and Amateur Musician"]
+        [:h4 {:style {:text-align "justify"}} "Hi there! This simple website showcases my personal portfolio. You can find my work in data science, machine learning, artificial intelligence and music here."]
+        [:h4 {:style {:text-align "justify"}} "You can also find fun web-based games here. The opponent you are playing with is trained using various methods in reinforcement learning and classical machine learning."]
+        [:h4 {:style {:text-align "justify"}} "This very website also showcases my web development skills. I built and designed it using the Luminus web framework."]
+        [:h4 "Enjoy!"]
+      ]
+      [:div.col-md-4.card-section.my-menu
+        [:h3 "My Work"]
+        [:a.disabled {:href "#/experiments"}
+          [:img {:src (str js/context "/img/experiments.png")}] "ML Experiments (coming soon)"]
+        [:a.disabled {:href "#/games"}
+          [:img {:src (str js/context "/img/games.png")}] "Games and AI (coming soon)"]
+        [:a {:href "https://musescore.com/user/25292271"}
+          [:img {:src (str js/context "/img/music.png")}] "Amateur Compositions"]
+      ]
+      [:div.col-md-4.card-section.my-menu
+        [:h3 "Links"]
+        [:a {:href "https://www.linkedin.com/in/ianchute/"} 
+          [:img {:src (str js/context "/img/social/linkedin.png")}] "LinkedIn"]
+        [:a {:href "https://github.com/ianchute"} 
+          [:img {:src (str js/context "/img/social/github.png")}] "GitHub"]
+        [:a {:href "https://www.quora.com/profile/Ian-Chu-Te"} 
+          [:img {:src (str js/context "/img/social/quora.png")}] "Quora"]
+        [:a {:href "https://www.youtube.com/channel/UCg_8JmfWVOHDC2xJdyZSvrQ"} 
+          [:img {:src (str js/context "/img/social/youtube.png")}] "YouTube"]
+        [:a {:href "https://twitter.com/yanchutz"} 
+          [:img {:src (str js/context "/img/social/twitter.png")}] "Twitter"]
+        [:a {:href "https://www.instagram.com/chutzzz"} 
+          [:img {:src (str js/context "/img/social/instagram.png")}] "Instagram"]
+        [:a {:href "https://web.facebook.com/chutzzz"} 
+          [:img {:src (str js/context "/img/social/facebook.png")}] "Facebook"]
+      ]
+  ]])
+
+(defn about-page []
+  [:div.timeline
+    [:div.container.left
+      [:div.content
+        [:h2 "2017"]
+        [:p "-"]]]
+    [:div.container.right
+      [:div.content
+        [:h2 "2016"]
+        [:p "-"]]]
+    [:div.container.left
+      [:div.content
+        [:h2 "2015"]
+        [:p "-"]]]
+    [:div.container.right
+        [:div.content
+          [:h2 "2014"]
+          [:p "-"]]]
+])
+
+(defn experiments-page []
+  [:div.container
+    [:div.row
+    [:div.col-md-12]]])
+
+(defn games-page []
+  [:div.container
+    [:div.row
+    [:div.col-md-12]]])
 
 (def pages
   {:home #'home-page
-   :about #'about-page})
+  :experiments #'experiments-page
+  :games #'games-page
+  :about #'about-page})
 
 (defn page []
   [(pages (session/get :page))])
@@ -54,6 +119,13 @@
 (secretary/defroute "/about" []
   (session/put! :page :about))
 
+(secretary/defroute "/experiments" []
+  (session/put! :page :experiments))
+
+(secretary/defroute "/games" []
+  (session/put! :page :games))
+  
+
 ;; -------------------------
 ;; History
 ;; must be called after routes have been defined
@@ -67,15 +139,11 @@
 
 ;; -------------------------
 ;; Initialize app
-(defn fetch-docs! []
-  (GET "/docs" {:handler #(session/put! :docs %)}))
-
 (defn mount-components []
   (r/render [#'navbar] (.getElementById js/document "navbar"))
   (r/render [#'page] (.getElementById js/document "app")))
 
 (defn init! []
   (load-interceptors!)
-  (fetch-docs!)
   (hook-browser-navigation!)
   (mount-components))
